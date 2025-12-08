87b3497c8d6934ccb6cfb83940dcc3bb6432caff
JacobCurtis <curtis.jac@northeastern.edu>
2025-12-07 21:16:50 -0500

    added correct paths for csv in config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-FMR_PATH = "/Users/jacobcurtis/Desktop/Final Project - DS 5010/data/Maine_FMR_2024.csv"
-WAGE_PATH = "/Users/jacobcurtis/Desktop/Final Project - DS 5010/data/maine_employment_wages_2024.csv"
-INCOME_PATH = "/Users/jacobcurtis/Desktop/Final Project - DS 5010/data/HDPulse_data_export.csv"
+FMR_PATH = "data/Maine_FMR_2024.csv"
+WAGE_PATH = "data/maine_employment_wages_2024.csv"
+INCOME_PATH = "data/HDPulse_data_export.csv"
